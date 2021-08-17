6df173b0351931b7eb7ec2f28c7039418c9f9e4b
Alan Hayes <ahayes@dotdash.com>
2021-08-17 15:31:02 -0400

    import csv

--- update_empolyeenumber.ps1
+++ update_empolyeenumber.ps1
@@ -1,2 +1,18 @@
-$csv_string = Read-Host -Prompt "Enter the path for the CSV"
-Import-Csv -Path $csv_string
+$name = update_employeenumber
+$date = Get-Date
+$log = "$name.txt"
+$home_dir = "~"
+$documents_dir = "Documents" 
+$downloads_dir = "Downloads"
+
+
+if ( -f $name.csv ) {
+    try { Import-Csv -Path ".\$name.csv" -ErrorAction Continue
+    } catch { $date + $_.Exception >> $log }
+} elseif ( -f "$home_dir\$documents_dir\$name.csv" ) {
+    try { Import-Csv -Path "$home_dir\$documents_dir\$name.csv" -ErrorAction Continue
+    } catch { $date + $_.Exception >> $log }   
+} elseif ( -f "$home_dir\$downloads\$name.csv" ) {
+    try { Import-Csv -Path "$home_dir\$downloads_dir\$name.csv" -ErrorAction Continue } 
+    catch { $date + $_.Exception >> $log }
+}
