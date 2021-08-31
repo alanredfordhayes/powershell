19064e05abaae8bdeffa1295322ea444c7fea0ee
Alan Hayes <ahayes@dotdash.com>
2021-08-31 11:26:29 -0400

    untouched csv

--- update_employeetitle.ps1
+++ update_employeetitle.ps1
@@ -108,28 +108,38 @@ function Update_Title {
     
 }
 
-$local_filename_check = Get-ChildItem -Path ".\" | Where-Object -Property Name -Match $filename
-$downloads_dir_check  = Get-ChildItem -Path "$home_dir\$downloads_dir" | Where-Object -Property Name -Match $filename
-$documents_dir_check = Get-ChildItem -Path "$home_dir\$documents_dir" | Where-Object -Property Name -Match $filename
+function copy_ToTemp {
+    param (
+        [String]$filename,
+        [String]$home_dir,
+        [String]$downloads_dir
+    )
+    $local_filename_check = Get-ChildItem -Path ".\" | Where-Object -Property Name -Match $filename
+    $downloads_dir_check  = Get-ChildItem -Path "$home_dir\$downloads_dir" | Where-Object -Property Name -Match $filename
+    $documents_dir_check = Get-ChildItem -Path "$home_dir\$documents_dir" | Where-Object -Property Name -Match $filename
 
-if ($null -ne $local_filename_check) {
-    $untouched_csv = $local_filename_check
-} elseif ($null -ne $downloads_dir_check) {
-    $untouched_csv = $downloads_dir_check
-} elseif ($null -ne $documents_dir_check) {
-    $untouched_csv = $documents_dir_check
-} else {
-    Write-Output "Cannot find file to import."
-    Write-Output "Please drop a file with name of $filename.csv in this directory or the following directories:"
-    Write-Output ".\$filename.csv"
-    Write-Output "$home_dir\$documents_dir\$filename.csv"
-    Write-Output "$home_dir\$downloads_dir\$filename.csv"
-    break
+    if ($null -ne $local_filename_check) {
+        $untouched_csv = $local_filename_check
+    } elseif ($null -ne $downloads_dir_check) {
+        $untouched_csv = $downloads_dir_check
+    } elseif ($null -ne $documents_dir_check) {
+        $untouched_csv = $documents_dir_check
+    } else {
+        Write-Output "Cannot find file to import."
+        Write-Output "Please drop a file with name of $filename.csv in this directory or the following directories:"
+        Write-Output ".\$filename.csv"
+        Write-Output "$home_dir\$documents_dir\$filename.csv"
+        Write-Output "$home_dir\$downloads_dir\$filename.csv"
+        break
+    }
+
+    if ($untouched_csv.GetType().BaseType.FullName -eq "System.Array") {
+        $untouched_csv = $untouched_csv | Sort-Object -Property LastWriteTime -Descending
+        $untouched_csv[0] | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv"
+    } else {
+        $untouched_csv | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv"
+    }
+    
 }
 
-if ($untouched_csv.GetType().BaseType.FullName -eq "System.Array") {
-    $untouched_csv = $untouched_csv | Sort-Object -Property LastWriteTime -Descending
-    $untouched_csv[0] | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv"
-} else {
-    $untouched_csv | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv"
-}
+copy_ToTemp -filename $filename -home_dir $home_dir -downloads_dir $downloads_dir
