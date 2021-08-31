39dbc51774e74989b359c288f571e1662b0f0a0a
Alan Hayes <ahayes@dotdash.com>
2021-08-31 11:38:11 -0400

    untouched csv

--- update_employeetitle.ps1
+++ update_employeetitle.ps1
@@ -5,45 +5,22 @@ $log = "$name.txt"
 $home_dir = "~"
 $documents_dir = "Documents" 
 $downloads_dir = "Downloads"
-$local_csv_path = ".\$name.csv"
-$document_csv_path = "$home_dir\$documents_dir\$name.csv"
-$download_csv_path = "$home_dir\$downloads_dir\$name.csv"
 
 function Import_CSV {
     param (
         [String]$name,
         [System.DateTime]$date,
-        [String]$log,
-        [String]$home_dir,
-        [String]$documents_dir,
-        [String]$downloads_dir,
-        [String]$local_csv_path,
-        [String]$document_csv_path,
-        [String]$download_csv_path
+        [String]$log
     )
-    
-    if ( Test-Path -Path $local_csv_path ) { $csv_path = $local_csv_path } 
-    elseif ( Test-Path -Path $document_csv_path  ) { $csv_path = $document_csv_path } 
-    elseif ( Test-Path -Path $download_csv_path ) { $csv_path = $download_csv_path } 
-    else {
-        Write-Output "Cannot find file to import."
-        Write-Output "Please drop a file with name of $name.csv in this directory or the following directories:"
-        Write-Output ".\$name.csv"
-        Write-Output "$home_dir\$downloads\$name.csv"
-        Write-Output "$home_dir\$documents_dir\$name.csv"
-        Write-Output "$home_dir\$downloads_dir\$name.csv"
-        break
-    }
-    
     if ( Test-Path -Path "$home_dir\AppData\Local\Temp\$name.csv") {
         try { Remove-Item -Path "$home_dir\AppData\Local\Temp\$name.csv" -ErrorAction Continue }
         catch { $Exception = $_.Exception ; "$date | $Exception " >> $log }
     }
 
-    $csv_content = Get-Content -Path $csv_path
+    $csv_content = Get-Content -Path "$home_dir\AppData\Local\Temp\$filename.csv"
     $csv_content | ForEach-Object {
         $line = $_
-        $first_line = Get-Content -Path $csv_path -First 1
+        $first_line = Get-Content -Path AppData\Local\Temp\$name.csv -First 1
         if ($line -eq $first_line){ 
             $line = $line.Replace(" ","_")
             $lineArray = $line.Split(",")
