631d744aa543111bce7de3e014072bb176b3229c
Alan Hayes <ahayes@dotdash.com>
2021-09-02 12:24:08 -0400

    update employeenumber new logic

--- update_employeenumber.ps1
+++ update_employeenumber.ps1
@@ -1,12 +1,10 @@
+$filename = "okta employee update"
 $name = "update_employeenumber"
 $date = Get-Date
 $log = "$name.txt"
 $home_dir = "~"
 $documents_dir = "Documents" 
 $downloads_dir = "Downloads"
-$local_csv_path = ".\$name.csv"
-$document_csv_path = "$home_dir\$documents_dir\$name.csv"
-$download_csv_path = "$home_dir\$downloads_dir\$name.csv"
 
 function Import_CSV {
     param (
@@ -14,42 +12,20 @@ function Import_CSV {
         [System.DateTime]$date,
         [String]$log,
         [String]$home_dir,
-        [String]$documents_dir,
-        [String]$downloads_dir,
-        [String]$local_csv_path,
-        [String]$document_csv_path,
-        [String]$download_csv_path
+        [String]$filename
     )
-    
-    if ( Test-Path -Path $local_csv_path ) { $csv_path = $local_csv_path } 
-    elseif ( Test-Path -Path $document_csv_path  ) { $csv_path = $document_csv_path } 
-    elseif ( Test-Path -Path $download_csv_path ) { $csv_path = $download_csv_path } 
-    else {
-        Write-Output "Cannot find file to import."
-        Write-Output "Please drop a file with name of $name.csv in this directory or the following directories:"
-        Write-Output ".\$name.csv"
-        Write-Output "$home_dir\$downloads\$name.csv"
-        Write-Output "$home_dir\$documents_dir\$name.csv"
-        Write-Output "$home_dir\$downloads_dir\$name.csv"
-        break
-    }
-    
+
     if ( Test-Path -Path "$home_dir\AppData\Local\Temp\$name.csv") {
         try { Remove-Item -Path "$home_dir\AppData\Local\Temp\$name.csv" -ErrorAction Continue }
         catch { $Exception = $_.Exception ; "$date | $Exception " >> $log }
     }
 
-    $csv_content = Get-Content -Path $csv_path
+    $csv_content = Get-Content -Path "$home_dir\AppData\Local\Temp\$filename.csv"
     $csv_content | ForEach-Object {
         $line = $_
-        $first_line = Get-Content -Path $csv_path -First 1
+        $first_line = Get-Content -Path "$home_dir\AppData\Local\Temp\$filename.csv" -First 1
         if ($line -eq $first_line){ 
             $line = $line.Replace(" ","_")
-            $lineArray = $line.Split(",")
-            $employee_Name = $lineArray[1]
-            $employee_NameArray = $Employee_Name.Split("_")
-            $employee_Name = $employee_NameArray[0].substring(1) + "_" + $employee_NameArray[1]
-            $line = $lineArray[0] + "," + $Employee_Name + "," + $lineArray[3] + "," + $lineArray[4] + "," + $lineArray[5]
         } 
 
         $line >> "$home_dir\AppData\Local\Temp\$name.csv"
@@ -57,55 +33,123 @@ function Import_CSV {
 
     $csv = Import-Csv "$home_dir\AppData\Local\Temp\$name.csv"
     return $csv
-
 }
 
-function Update_EmployeeNumber {
+function Update_Title {
     param (
         [System.Array]$csv
     )
 
-    $users_list = Get-AdUser -Filter * -Properties mail, employeenumber, proxyAddresses
+    function set_title {
+        param (
+            [Microsoft.ActiveDirectory.Management.ADAccount]$aduser,
+            [String]$csv_title,
+            [String]$csv_employee_name,
+            $bool
+        )
+        
+        if ($aduser.Title -ne $csv_title) { 
+            Write-Output "UPDATE: Since Employee Title for USER: $csv_employee_name is $bool updating TITLE..."
+            try { Set-ADUser -Identity $aduser.SamAccountName -Title $csv_title -ErrorAction Continue }
+            catch { $Exception = $_.Exception ; "$date | $Exception " >> $log; Write-Output "ERROR: Check Log" }
+            Write-Output "Done"
+        } else {
+            Write-Output "GOOD: Since Employee Title for USER: $csv_employee_name is $bool NOT updating TITLE"
+        }
+
+    }
+
+    $ADUsers = Get-ADUser -Filter * -Properties mail, Title, targetAddress
 
     $csv | ForEach-Object {
-        $employeenumber = $_.Employee_Number
-        $employeename = $_.Employee_Name 
-        $email_address = $_.Email_Address
-        $user = $users_list | Where-Object -Property mail -eq $email_address
-        if ($null -eq $user) {
-            $email_address_array = $email_address.Split("@")
-            $upn = $email_address_array[0] + "@dash.corp"
-            $user = $users_list | Where-Object -Property UserPrincipalName -eq $upn
-        }
-
-        if ($null -eq $user) {
-            $users_list | ForEach-Object {
-                $proxyAddresses = $_.proxyAddresses
-                $smtp = "smtp:$email_address"
-                if ($proxyAddresses.Contains($smtp)) {
-                    $user = $_
-                }
-            }
-        } 
-
-        if (($null -ne $user) -and ($user.gettype().basetype.name -eq "Array")) {
-            $user = $user | Where-Object -Property Enabled -eq "True"
-        }
-
-        if ($user.employeenumber -ne $employeenumber) {
-            if ($null -ne $user) {
-                Write-Output "Updating User: $employeename"
-                try { Set-AdUser $user.DistinguishedName -EmployeeNumber $employeenumber -ErrorAction Continue }
-                catch { Write-Output "Error on User: $employeename" ; $Exception = $_.Exception ; "$date | $employeename | $Exception " >> $log }
+        $csv_email_address = $_.Email_Address
+        $csv_title = $_.Job_Title
+        $csv_employee_name = $_.Employee_Name
+        $aduser = $ADUsers | Where-Object -Property mail -EQ $csv_email_address
+        if ($null -ne $aduser) {
+            Write-Output "Info: Found USER: $csv_employee_name based on CSV comparison."
+            if ($aduser.GetType().BaseType.Name -ne "Array") {
+                $bool_employee_title = $aduser.Title -ne $csv_title
+                set_title -aduser $aduser -csv_title $csv_title -csv_employee_name $csv_employee_name -bool $bool_employee_title
             } else {
-                Write-Output "Error finding: $employeename" ; "$date | $employeename | User NOT Found. " >> $log 
+                Write-Output "Warning: Found multiple entries for USER: $csv_employee_name"
+                $aduser = $aduser | Where-Object -Property Enabled -eq "True"
+                $bool_employee_title = $aduser.Title -ne $csv_title
+                set_title -aduser $aduser -csv_title $csv_title -csv_employee_name $csv_employee_name -bool $bool_employee_title
             }
         } else {
-            Write-Output "EmployeeNumber for User: $employeename is Good."
+            Write-Output "WARNING: Could not find USER: $csv_employee_name based on Email Address from CSV"
+            $aduser = $ADUsers | Where-Object -Property targetAddress -EQ "SMTP:$csv_email_address"
+            if ($null -ne $aduser) {
+                Write-Output "Info: Found USER: $csv_employee_name based on Target Address"
+                $bool_employee_title = $aduser.Title -ne $csv_title
+                set_title -aduser $aduser -csv_title $csv_title -csv_employee_name $csv_employee_name -bool $bool_employee_title
+            } else {
+                Write-Output "WARNING: Could not find USER: $csv_employee_name based on Target Addreess from CSV"
+                $csv_email_address_array = $csv_email_address.Split("@")
+                try { $aduser = Get-ADUser $csv_email_address_array[0] -Properties mail, Title -ErrorAction Continue }
+                catch { $Exception = $_.Exception ; "$date | $Exception " >> $log; Write-Output "ERROR: Check Log" }
+                $bool_aduser_query = $null -ne $aduser
+                if ($null -ne $aduser) {
+                    Write-Output "Info: Since estimated SamAccountName for USER: $csv_employee_name is $bool_aduser_Query"
+                    set_title -aduser $aduser -csv_title $csv_title -csv_employee_name $csv_employee_name -bool $bool_aduser_Query
+                } else {
+                    Write-Output "WARNING: Since estimated SamAccountName for USER: $csv_employee_name is $bool_aduser_Query executing additional search."
+                    $dn = "CN=$csv_employee_name,OU=Users_OU,DC=dash,DC=corp"
+                    try { $aduser = Get-ADUser $dn -Properties mail, Title -ErrorAction Continue }
+                    catch { $Exception = $_.Exception ; "$date | $Exception " >> $log; Write-Output "ERROR: Check Log" }
+                    $bool_dn_query = $null -ne $aduser
+                    if ($null -ne $aduser) {
+                        Write-Output "Info: Since estimated Distinguished Name for USER: $csv_employee_name is $bool_dn_query"
+                        set_title -aduser $aduser -csv_title $csv_title -csv_employee_name $csv_employee_name -bool $bool_dn_query
+                    } else {
+                        Write-Output "WARNING: Since estimated Distinguished Name for USER: $csv_employee_name is $bool_dn_query executing addtional search."
+                    }
+                }
+            }
         }
+    }
+}
+
+function copy_ToTemp {
+    param (
+        [String]$filename,
+        [String]$home_dir,
+        [String]$downloads_dir
+    )
+    $local_filename_check = Get-ChildItem -Path ".\" | Where-Object -Property Name -Match $filename
+    $downloads_dir_check  = Get-ChildItem -Path "$home_dir\$downloads_dir" | Where-Object -Property Name -Match $filename
+    $documents_dir_check = Get-ChildItem -Path "$home_dir\$documents_dir" | Where-Object -Property Name -Match $filename
+
+    if ($null -ne $local_filename_check) {
+        $untouched_csv = $local_filename_check
+    } elseif ($null -ne $downloads_dir_check) {
+        $untouched_csv = $downloads_dir_check
+    } elseif ($null -ne $documents_dir_check) {
+        $untouched_csv = $documents_dir_check
+    } else {
+        Write-Output "Cannot find file to import."
+        Write-Output "Please drop a file with name of $filename.csv in this directory or the following directories:"
+        Write-Output ".\$filename.csv"
+        Write-Output "$home_dir\$documents_dir\$filename.csv"
+        Write-Output "$home_dir\$downloads_dir\$filename.csv"
+        break
+    }
+
+    if ( Test-Path -Path "$home_dir\AppData\Local\Temp\$filename.csv") {
+        try { Remove-Item -Path "$home_dir\AppData\Local\Temp\$filename.csv" -ErrorAction Continue }
+        catch { $Exception = $_.Exception ; "$date | $Exception " >> $log }
+    }
+
+    if ($untouched_csv.GetType().BaseType.FullName -eq "System.Array") {
+        $untouched_csv = $untouched_csv | Sort-Object -Property LastWriteTime -Descending
+        $untouched_csv[0] | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv" -Force
+    } else {
+        $untouched_csv | Copy-Item -Destination "$home_dir\AppData\Local\Temp\$filename.csv" -Force -
     }
     
 }
 
-$csv = Import_CSV -name $name -date $date -log $log -home_dir $home_dir -documents_dir $documents_dir -downloads_dir $downloads_dir -local_csv_path $local_csv_path -document_csv_path $document_csv_path -download_csv_path $download_csv_path
-Update_EmployeeNumber -csv $csv
+copy_ToTemp -filename $filename -home_dir $home_dir -downloads_dir $downloads_dir
+$csv = Import_CSV -name $name -date $date -log $log -home_dir $home_dir -filename $filename
+Update_Title -csv $csv
