2240b76d590d4082b0f8c35d3198bb6107b45575
Alan Hayes <ahayes@dotdash.com>
2021-10-07 05:33:43 -0400

    get-security group

--- change_employeelocation.ps1
+++ change_employeelocation.ps1
@@ -1,6 +1,7 @@
 $scriptName = $MyInvocation.MyCommand.Name
 $path = Get-Location
 $scriptLog = "$path\$scriptName.log"
+
 function errorHandling {
     param ([System.Management.Automation.ErrorRecord]$handledError, [System.String]$scriptLog )
     $date = Get-Date
@@ -14,7 +15,21 @@ function errorHandling {
 }
 
 try { $adSecurityGroup = Read-Host -Prompt "AD Security Grouup" -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Read-Host | AD Security Grouup " }
-if ($null -eq $adSecurityGroup) { Write-Output "Response was null. Quitting..." ; exit 1 }
-else { 
-    try { $adGroup = Get-AdGroup -Identity $adSecurityGroup -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Get-AdGroup" } }
+try { $adGroup = Get-AdGroup -Identity $adSecurityGroup -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Get-AdGroup" } }
+try { $adGroupMembers = Get-ADGroupMember -Identity $adGroup -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Get-AdGroupMember" }
+$adGroupMembers | ForEach-Object {
+    $distinguishedName   = $_.distinguishedName
+    $name                = $_.name
+    $objectClass         = $_.objectClass
+    $objectGUID          = $_.objectGUID
+    $SamAccountName      = $_.SamAccountName
+    $SID                 = $_.SID
 
+    $Office                      = "28 Liberty St, Floor 7, New York, NY 10005"
+    $physicalDeliveryOfficeName  = "28 Liberty St, Floor 7, New York, NY 10005"
+    $PostalCode                  = "10005"
+    $StreetAddress               = "28 Liberty St, Floor 7, New York, NY 10005"
+
+    try { $adUser = Get-ADUser -Identity $SamAccountName -Properties Office, physicalDeliveryOfficeName, PostalCode, StreetAddress -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Get-AdGroupUser" }
+    $adUser
+}
