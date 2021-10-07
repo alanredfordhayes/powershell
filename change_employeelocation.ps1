df4ad9ccdbaa8566773851af62d6c406f1dbde43
Alan Hayes <ahayes@dotdash.com>
2021-10-07 05:14:59 -0400

    write error | exit on null

--- change_employeelocation.ps1
+++ change_employeelocation.ps1
@@ -1,5 +1,17 @@
-Import-Module .\global_vars.ps1
-Import-Module .\global_functions.ps1
+$scriptName = $MyInvocation.MyCommand.Name
+$path = Get-Location
+$scriptLog = "$path\$scriptName.log"
+function errorHandling {
+    param ([System.Management.Automation.ErrorRecord]$handledError, [System.String]$scriptLog )
+    $date = Get-Date
+    $message = $handledError.Exception.Message
+    $categoryInfo = $handledError.CategoryInfo
+    $category = $categoryInfo.Category
+    $activity = $categoryInfo.Activity
+    $reason = $categoryInfo.Reason
+    Write-Warning $message
+    Write-Output "$date | azContext | $category on $activity caused by $reason. | $message" >> $scriptLog
+}
 
 try { $adSecurityGroup = Read-Host -Prompt "AD Security Grouup" -ErrorAction Stop } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Read-Host | AD Security Grouup " }
 if ($null -eq $adSecurityGroup) { Write-Output "Response was null. Quitting..." ; exit 1 }
