779a1de12a73e2531970e7249a436fa86eae7ee9
Alan Hayes <ahayes@dotdash.com>
2021-10-07 05:08:25 -0400

    write error | exit on null

--- change_employeelocation.ps1
+++ change_employeelocation.ps1
@@ -2,5 +2,5 @@ Import-Module .\global_vars.ps1
 Import-Module .\global_functions.ps1
 
 try { $adSecurityGroup = Read-Host -Prompt "AD Security Grouup" } catch { errorHandling -handledError $Error[0] -scriptLog $scriptLog -name "Read-Host | AD Security Grouup " }
-$adSecurityGroup
+if ($null -eq $adSecurityGroup) { Write-Error -Message "Response was null. Quitting..." ; exit 1 }
 
